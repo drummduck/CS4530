9c56d611859a88887478a9169c9b3ee7c86da171
drummduck <nathan.donaldsondrum@gmail.com>
2017-11-13 23:08:30 -0700

    BattleFire: Finishing up login page, need to work on savedState, then need to work on logout button for recycler view then JSON stuff

--- Assignments/Battleship/app/src/main/java/com/example/natha/battleship/Login.kt
+++ Assignments/Battleship/app/src/main/java/com/example/natha/battleship/Login.kt
@@ -1,16 +1,17 @@
 package com.example.natha.battleship
 
 import android.content.ContentValues.TAG
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
 import android.support.annotation.NonNull
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Gravity
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.view.ViewGroup
+import android.widget.*
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
@@ -24,16 +25,19 @@ import kotlinx.android.synthetic.main.login.*
  * Created by Natha on 11/12/2017.
  */
 class Login : AppCompatActivity() {
-
     lateinit var emailField : EditText
     lateinit var passwordField : EditText
     lateinit var errorField : TextView
     lateinit var loginButton : Button
+    lateinit var registerButton : Button
     lateinit var forgotPassword : TextView
+    lateinit var title : TextView
     lateinit var auth : FirebaseAuth
     lateinit var currentUser : FirebaseUser
     lateinit var task : Task<AuthResult>
-    var triedSignIn = false
+    var loginState = LoginState.STARTED
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,16 +46,28 @@ class Login : AppCompatActivity() {
         emailField = findViewById(R.id.email)
         passwordField = findViewById(R.id.password)
         errorField = findViewById(R.id.errorMessage)
-        loginButton = findViewById(R.id.loginRegister)
+        loginButton = findViewById(R.id.login)
+        registerButton = findViewById(R.id.register)
         forgotPassword = findViewById(R.id.forgot)
+        title = findViewById(R.id.title)
 
         loginButton.setOnClickListener(clickListener)
+        registerButton.setOnClickListener(clickListener)
         forgotPassword.setOnClickListener(clickListener)
         auth = FirebaseAuth.getInstance()
-        if(auth.currentUser != null)
+        if(auth != null && auth.currentUser != null)
         {
+            Log.e("LOGIN", "Current user is logged in")
             currentUser = auth.currentUser!!
-            //start activity, give user info, this is where it begins to suck
+        }
+        if(auth != null && auth.currentUser != null && currentUser.isEmailVerified)
+        {
+            Log.e("LOGIN", "Current user exists and has email verified ")
+            intent = Intent(applicationContext, Game::class.java)
+            intent.putExtra("userEmail", currentUser.email)
+            intent.putExtra("userUID", currentUser.uid)
+            startActivity(intent)
+            finish()
         }
     }
 
@@ -59,47 +75,222 @@ class Login : AppCompatActivity() {
 
         when(view.id)
         {
-            R.id.loginRegister ->
+            R.id.login ->
             {
-                if(passwordField.text.contains(("^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#?!@\$%^&*-]).{8,}$").toRegex()) && !passwordField.text.isEmpty() && !loginRegister.text.isEmpty()) {
+                loginState = LoginState.SIGNING_IN
+                if(passwordField.text.contains(("^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#:?!@\$%^&*-]).{8,}$").toRegex()) && !passwordField.text.isEmpty() && !emailField.text.isEmpty()) {
+                    Log.e("LOGIN", "Attempting to sign in first")
                     auth.signInWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener(onCompleteListener)
-                    if (triedSignIn) {
-                        auth.createUserWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener(onCompleteListener)
-                        triedSignIn = false
-                    }
                 }
                 else
                 {
+                    loginState = LoginState.STARTED
+                    Log.e("LOGIN", "Fields are either empty or the password doesnt meet the criteria")
                     errorField.setBackgroundColor(Color.RED)
-                    if(passwordField.text.isEmpty() || loginRegister.text.isEmpty())
+                    if(passwordField.text.isEmpty() || emailField.text.isEmpty())
                         errorField.setText("Password and Email fields must not be empty")
                     else
                         errorField.setText("Password requires 1 or more capital letters, digits, and non-alphanumeric symbols and must be at least 8 characters")
                 }
             }
 
+            R.id.register ->
+            {
+                loginState = LoginState.CREATING
+                if(passwordField.text.contains(("^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[#:?!@\$%^&*-]).{8,}$").toRegex()) && !passwordField.text.isEmpty() && !emailField.text.isEmpty()) {
+                    Log.e("LOGIN", "Attempting to create user")
+                    auth.createUserWithEmailAndPassword(emailField.text.toString(), passwordField.text.toString()).addOnCompleteListener(onCompleteListener)
+                }
+                else
+                {
+                    loginState = LoginState.STARTED
+                    Log.e("LOGIN", "Fields are either empty or the password doesnt meet the criteria")
+                    errorField.setBackgroundColor(Color.RED)
+                    if(passwordField.text.isEmpty() || emailField.text.isEmpty())
+                        errorField.setText("Password and Email fields must not be empty")
+                    else
+                        errorField.setText("Password requires 1 or more capital letters, digits, and non-alphanumeric symbols and must be at least 8 characters")
+                }            }
+
             R.id.forgot ->
             {
-                auth.signOut()
+                if(forgotPassword.text.equals("Cancel"))
+                {
+                    emailField.text.clear()
+                    emailField.hint = "Email:"
+                    emailField.gravity = Gravity.LEFT
+                    emailField.isFocusable = true
+                    passwordField.text.clear()
+                    passwordField.hint = "Password:"
+                    forgotPassword.text = "Forgot Password?"
+                    errorField.setText("Email verifcation not completed.")
+                    errorField.visibility = View.VISIBLE
+                    passwordField.visibility = View.VISIBLE
+                    title.visibility = View.VISIBLE
+                    loginButton.visibility = View.VISIBLE
+                    registerButton.visibility = View.VISIBLE
+                }
+                //else do forgot password shit
             }
         }
     }
 
     val onCompleteListener = OnCompleteListener<AuthResult>{ task ->
 
-        if(task.isSuccessful)
-        {
-            if(triedSignIn){}//Do verification?
-            //Go to game list
+        if(task.isSuccessful) {
+            Log.e("Login", "Successful login or creation")
+            auth = FirebaseAuth.getInstance()
+            if (auth != null && auth.currentUser != null) {
+                Log.e("LOGIN", "User exists on successful login or creation")
+                currentUser = auth.currentUser!!
+            }
+
+            if (loginState == LoginState.SIGNING_IN) {
+                if (currentUser.isEmailVerified) {
+                    Log.e("LOGIN", "Email is verified on sign in, take to main screen")
+                    intent = Intent(applicationContext, Game::class.java)
+                    intent.putExtra("userEmail", currentUser!!.email)
+                    intent.putExtra("userUID", currentUser!!.uid)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    emailField.setText("Please verify your email.")
+                    emailField.gravity = Gravity.CENTER
+                    emailField.isFocusable = false
+                    forgotPassword.setText("Cancel")
+                    errorField.visibility = View.INVISIBLE
+                    loginButton.visibility = View.INVISIBLE
+                    registerButton.visibility = View.INVISIBLE
+                    passwordField.visibility = View.INVISIBLE
+                    title.visibility = View.INVISIBLE
+                    sendVerificationEmail()
+                    var timer = 0
+                    for(i in 1..300) {
+                        Handler().postDelayed(Runnable {
+                            if (timer >= 300 && forgotPassword.text.equals("Cancel")) {
+                                Log.e("LOGIN", "Times up, kicking back to login screen")
+                                emailField.text.clear()
+                                emailField.hint = "Email:"
+                                emailField.gravity = Gravity.LEFT
+                                emailField.isFocusable = true
+                                passwordField.text.clear()
+                                passwordField.hint = "Password:"
+                                forgotPassword.text = "Forgot Password?"
+                                errorField.setText("Email verifcation not completed.")
+                                errorField.visibility = View.VISIBLE
+                                passwordField.visibility = View.VISIBLE
+                                title.visibility = View.VISIBLE
+                                loginButton.visibility = View.VISIBLE
+                                registerButton.visibility = View.VISIBLE
+
+                                auth.signOut()
+                            } else {
+                                if (currentUser.isEmailVerified) {
+                                    Log.e("LOGIN", "Email is verified, take to main screen")
+                                    intent = Intent(applicationContext, Game::class.java)
+                                    intent.putExtra("userEmail", currentUser!!.email)
+                                    intent.putExtra("userUID", currentUser!!.uid)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                            }
+                        }, timer.toLong() * 1000)
+                        timer = timer++
+                        if(!forgotPassword.text.equals("Cancel")) break
+                    }
+                }
+            } else if (loginState == LoginState.CREATING) {
+                emailField.setText("Please verify your email.")
+                emailField.isFocusable = false
+                emailField.gravity = Gravity.CENTER
+                forgotPassword.setText("Cancel")
+                errorField.visibility = View.INVISIBLE
+                loginButton.visibility = View.INVISIBLE
+                registerButton.visibility = View.INVISIBLE
+                passwordField.visibility = View.INVISIBLE
+                title.visibility = View.INVISIBLE
+                var timer = 0
+                for(i in 1..300) {
+                    Handler().postDelayed(Runnable {
+                        if (timer >= 300 && forgotPassword.text.equals("Cancel")) {
+                            Log.e("LOGIN", "Times up, kicking back to login screen")
+                            emailField.text.clear()
+                            emailField.hint = "Email:"
+                            emailField.gravity = Gravity.LEFT
+                            emailField.isFocusable = true
+                            passwordField.text.clear()
+                            passwordField.hint = "Password:"
+                            forgotPassword.text = "Forgot Password?"
+                            errorField.setText("Email verifcation not completed.")
+                            errorField.visibility = View.VISIBLE
+                            passwordField.visibility = View.VISIBLE
+                            title.visibility = View.VISIBLE
+                            loginButton.visibility = View.VISIBLE
+                            registerButton.visibility = View.VISIBLE
+
+                            auth.signOut()
+                        } else {
+                            if (currentUser.isEmailVerified) {
+                                Log.e("LOGIN", "Email is verified, take to main screen")
+                                intent = Intent(applicationContext, Game::class.java)
+                                intent.putExtra("userEmail", currentUser!!.email)
+                                intent.putExtra("userUID", currentUser!!.uid)
+                                startActivity(intent)
+                                finish()
+                            }
+                        }
+                    }, timer.toLong() * 1000)
+                    timer = timer++
+                    if(!forgotPassword.text.equals("Cancel")) break
+                }
+            }
         }
 
         else
         {
-            if(triedSignIn)Toast.makeText(this, "Authentication failed.",
-                    Toast.LENGTH_SHORT).show()
-            triedSignIn = true
+            auth.signOut()
+            errorField.text = "Authentication failed"
+            if(loginState == LoginState.SIGNING_IN) errorField.text = errorField.text.toString() + ", user may not exist or check internet settings."
+            else if(loginState == LoginState.CREATING)  errorField.text = errorField.text.toString() + ", check internet settings."
         }
+    }
+
+    private fun sendVerificationEmail()
+    {
+        Log.e("LOGIN", "Sending verification email")
+        loginState = LoginState.VERIFYING
+        if(currentUser != null)currentUser.sendEmailVerification().addOnCompleteListener(OnCompleteListener() { task ->
+
+            if(!task.isSuccessful && forgotPassword.text.equals("Cancel"))
+            {
+                errorField.text = "Email verification failed to send, check your internet settings."
+                emailField.text.clear()
+                emailField.hint = "Email:"
+                emailField.gravity = Gravity.LEFT
+                emailField.isFocusable = true
+                passwordField.text.clear()
+                passwordField.hint = "Password:"
+                forgotPassword.text = "Forgot Password?"
+                errorField.visibility = View.VISIBLE
+                passwordField.visibility = View.VISIBLE
+                title.visibility = View.VISIBLE
+                loginButton.visibility = View.VISIBLE
+                registerButton.visibility = View.VISIBLE
+
+            }
+        })
+    }
+
+    public override fun onSaveInstanceState(savedInstanceState: Bundle?) {
+        if(savedInstanceState !is Bundle) return
+        Log.e("SAVEDINSTANCESTATE", "IN SAVED INSTANCE STATE BEFORE")
+        savedInstanceState.putString("loginState", fileName)
+        super.onSaveInstanceState(savedInstanceState)
     }
 }
 
 
+enum class LoginState
+{
+    STARTED, SIGNING_IN, CREATING, VERIFYING, DONE
+}
